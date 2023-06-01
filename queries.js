bfe070093579298f4e110971e7089be82fe60f54
Cristian Ramírez <cristian.ramirez.cl@gmail.com>
2023-05-31 22:54:24 -0400

    Creando posts

--- queries.js
+++ queries.js
@@ -3,11 +3,34 @@ const pool = require( './bd' );
 // traer todos los posts de la bd
 const getPosts = async () => {
   const { rows } = await pool.query( "SELECT * FROM posts" )
-  console.log( rows )
   return rows
 }
 
+// crear un nuevo post
+const createPost = async ( payload ) => {
+  const consulta = "INSERT INTO posts values (DEFAULT, $1, $2, $3)"
+  const { titulo, img, descripcion } = payload
+  const values = [ titulo, img, descripcion ]
+  const result = await pool.query( consulta, values )
+}
+
+
+// actualizar un post
+const updatePost = async ( id, post ) => {
+  //
+}
+
+
+// eliminar un post
+const deletePost = async ( id ) => {
+  //
+}
+
+
 module.exports = {
-  getPosts
+  getPosts,
+  createPost,
+  updatePost,
+  deletePost
 }
 
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require( 'dotenv' ).config()
 const express = require( 'express' )
 const app = express()
 const cors = require( 'cors' )
-const { getPosts } = require( './queries' )
+const { getPosts, createPost, updatePost, deletePost } = require( './queries' )
 
 const port = process.env.PORT || 3001
 app.listen( port, console.log( `Server running on http://localhost:${ port }` ) )
@@ -20,4 +20,12 @@ app.get( '/posts', async ( req, res ) => {
   } catch ( error ) {
     res.status( 500 ).json( { error: error.message } )
   }
+} )
+
+// POST
+app.post( '/posts', async ( req, res ) => {
+  const { titulo, img, descripcion } = req.body
+  await createPost( { titulo, img, descripcion } )
+  res.status( 200 ).json( { message: 'Post creado' } )
+  console.log( 'Post creado' )
 } )
